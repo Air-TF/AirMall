c56c2b8bab6c69e9a0894a6fb9a0b14dba3c9324
Air-TF <1318860027@qq.com>
2019-05-20 21:53:30 +0800

    [添加]权限管理、安全中心页面 [修改]用户中心页面

--- app/src/main/java/com/example/airmall/activity/UserInfoActivity.java
+++ app/src/main/java/com/example/airmall/activity/UserInfoActivity.java
@@ -9,26 +9,28 @@ import android.widget.Button;
 import com.example.airmall.R;
 import com.example.airmall.utils.SPUtils;
 import com.example.airmall.weight.section.SectionTextItemView;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 public class UserInfoActivity extends AppCompatActivity {
 
     private Button btn_logout;
     private SectionTextItemView tv_userId;
+    private SimpleDraweeView img_account;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
-
         initView();
     }
 
     private void initView() {
         btn_logout = findViewById(R.id.btn_logout);
         tv_userId = findViewById(R.id.tv_userId);
+        img_account= findViewById(R.id.img_account);
 
-        tv_userId.setContextTxt((String) SPUtils.get(UserInfoActivity.this,"userId",""));
+        tv_userId.setContextTxt((String) SPUtils.get(UserInfoActivity.this, "userId", ""));
 
         btn_logout.setOnClickListener(v -> {
             Intent intent = new Intent(UserInfoActivity.this, LoginActivity.class);
